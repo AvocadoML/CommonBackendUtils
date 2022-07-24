36bb3eb6d58f7a205caf10c5632c98565ae4c836
Maciej Kozarzewski <mk@eskrzynka.pl>
2022-07-24 14:35:39 +0200

    Added benchmarking utils.

---
+++ benchmark/arg_parser.hpp
@@ -0,0 +1,19 @@
+/*
+ * arg_parser.hpp
+ *
+ *  Created on: May 18, 2022
+ *      Author: Maciej Kozarzewski
+ */
+
+#ifndef ARG_PARSER_HPP_
+#define ARG_PARSER_HPP_
+
+namespace avocado
+{
+	namespace backend
+	{
+
+	} /* namespace backend */
+} /* namespace avocado */
+
+#endif /* ARG_PARSER_HPP_ */
